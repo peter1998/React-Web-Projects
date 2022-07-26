341cd14a6923657193e12ccaad59c24260ac8a1c
Peter Matov <pmatov@gmail.com>
2022-07-26 22:08:37 +0300

    seafood api page with React.js

---
+++ meals/src/Body.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+import './Body.css'
+
+function Body(){
+    const[items,setitems] = useState([])
+    
+    useEffect(()=> {
+         axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+         .then(res=>{
+                
+            setitems(res.data.meals)
+
+         }).catch(err=>{
+
+         })
+    }, [])
+
+    const itemslist = items.map((obj)=>{
+        return <div className='col-md-4'>
+            <p>{obj.strMeal}</p>
+            <img src = {obj.strMealThumb} className='img-fluid'/>
+            <p>{obj.idMeal}</p>
+        </div>
+    })
+
+
+    return <div>
+        <div className='row'>
+            {itemslist}
+        </div>
+          </div>
+}
+
+export default Body
---
+++ meals/src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+    return <div style={{backgroundColor: 'brown'}}>
+        <p style={{color: 'white'}}>Designed and Developed By</p>
+        <p style={{color: 'white'}}>P.Matov</p>
+    </div>
+}
+
+export default Footer
