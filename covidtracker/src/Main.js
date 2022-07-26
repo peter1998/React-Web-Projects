bed04206046fdadb7113feee17b5cfd03fb6b3ec
Peter Matov <pmatov@gmail.com>
2022-07-26 19:51:56 +0300

    Covid Status Table React Page App

---
+++ covidtracker/src/Main.js
@@ -0,0 +1,63 @@
+import React, { useEffect , useState } from 'react'
+import axios from 'axios'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import 'jquery/dist/jquery.min.js'
+//Datatables Modules
+import "datatables.net-dt/js/dataTables.dataTables"
+import "datatables.net-dt/css/jquery.dataTables.min.css"
+import $ from 'jquery';
+
+export default function Main() {
+
+
+const [coviddata, setcoviddata]= useState([])
+
+useEffect(()=>{
+
+axios.get('https://api.covid19api.com/summary').then(res=>{
+    console.log(res.data)
+    setcoviddata(res.data.Countries)
+}).catch(err=>{console.log(err)})
+
+ $(document).ready(()=>{
+    $('#mytable').DataTable()
+ })
+}, [])
+
+const tabledata = coviddata.map(obj=>{
+    return <tr>
+        <td>{obj.Country}</td>
+        <td>{obj.TotalConfirmed}</td>
+        <td>{obj.TotalConfirmed-obj.TotalRecovered}</td>
+        <td>{obj.TotalRecovered}</td>
+        <td>{obj.TotalDeaths}</td>
+    </tr>
+})
+
+    return (
+        <div>
+            <h1>CovidStatus</h1>
+
+            <div className='row justify-content-center'>
+                <div className='col-md-8'>
+                    <table id='mytable' className='table table-bordered'>
+                        <thead>
+                            <tr>
+                                <th>Country</th>
+                                <th>Confirmed</th>
+                                <th>Active</th>
+                                <th>Recovered</th>
+                                <th>Deaths</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {tabledata}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
